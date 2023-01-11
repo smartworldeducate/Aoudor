dc1c59f95cb22d65d0a7450e7586f13c4c2faae3
Aswin-Sudhakaran <aswinsudhakaran98@gmail.com>
2023-01-11 20:39:02 +0530

    rating-update-11/1/23

--- models/productModel.js
+++ models/productModel.js
@@ -50,9 +50,14 @@ var productSchema = new mongoose.Schema({
     },
     ratings:[{
         star:Number,
+        comment:String,
         postedby:{type:mongoose.Schema.Types.ObjectId,ref:"User"},
 
     }],
+    totalrating:{
+        type:String,
+        default:0,
+    }
 
 
     
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,5 +1,5 @@
 const express=require('express');
-const { createProduct, getaProduct, getAllProduct, updateProduct, deleteProduct, AddtoWishlist } = require('../controller/productCtrl');
+const { createProduct, getaProduct, getAllProduct, updateProduct, deleteProduct, AddtoWishlist, rating } = require('../controller/productCtrl');
 const {isAdmin,authMiddleware} =require('../middlewares/authMiddleware')
 const router=express.Router();
 
@@ -7,6 +7,7 @@ const router=express.Router();
 router.post('/',authMiddleware,createProduct,isAdmin)
 router.get('/:id',getaProduct)
 router.put('/wishlist',authMiddleware,AddtoWishlist)
+router.put('/rating',authMiddleware,rating)
 router.put('/:id',authMiddleware,updateProduct,isAdmin)
 router.delete('/:id',authMiddleware,deleteProduct,isAdmin)
 router.get('/',getAllProduct)
